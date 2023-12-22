1a8b9826c2967af89b34f51b204873524d47520d
Dev-Iyke <iyke.thompson01@gmail.com>
2023-12-22 13:11:30 +0100

    python modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    length = len(sys.argv) -1
+    if length == 0:
+        print("{} arguments.".format(length))
+    elif length == 1 or length > 1:
+        print("{} argument:".format(length))
+    
+    for i in range(length):
+        print("{}: {}".format((i + 1), sys.argv[i + 1]))
