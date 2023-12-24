8a6d7f25c5a383be1b4d998adfe67cd795a8789a
Dev-Iyke <iyke.thompson01@gmail.com>
2023-12-24 16:50:43 +0100

    python data structures and tuples

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,8 +6,9 @@ def max_integer(my_list=[]):
     length = len(my_list) - 1
     while length > 0:
         j = 0
-        while j < length: 
-            """ this loop is to check where that particular numbeer at the index of j after the current iteration """
+        while j < length:
+            """ this loop is to check where that particular number... """
+            """ at the index of j after the current iteration """
             """ checking which value is greater """
             if my_list[j] > my_list[j + 1]:
                 """ storing the current value and exchanging position """
