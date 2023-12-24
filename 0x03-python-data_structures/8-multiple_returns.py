0cc7d7ca65629f03c2932a77d16b81b3bc6e9691
Dev-Iyke <iyke.thompson01@gmail.com>
2023-12-24 15:48:24 +0100

    python data structures and tuples

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     length = len(sentence)
     if sentence == '':
-        sentence[0] = 'None'
+        sentence[0] = None
     else:
         first = sentence[0]
     return (length, first)
