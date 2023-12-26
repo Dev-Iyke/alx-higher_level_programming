445449a1a578f592ee3267eee6f0639137ac70a9
Dev-Iyke <iyke.thompson01@gmail.com>
2023-12-26 14:41:35 +0100

    More on python data structures - keys and values in a dictionary

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    f = {key: value * 2 for key,value in a_dictionary.items()}
+    f = {key: value * 2 for key, value in a_dictionary.items()}
     return (f)
