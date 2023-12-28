acd2cadcc93b40034a9102f6c8486b3ccbe1c150
Dev-Iyke <iyke.thompson01@gmail.com>
2023-12-28 19:55:12 +0100

    python data structures - roman to integers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,8 +8,5 @@ def roman_to_int(roman_string):
         roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
         for i in reversed(roman_string):
             num = roman[i]
-            if result < num * 5:
-                result += num
-            else:
-                result - num
+            result += num if result < num * 5 else -num
         return (result)
