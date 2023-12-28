a00b8788b1ff9ae15132e6c6e7e1611b20b5c2bd
Dev-Iyke <iyke.thompson01@gmail.com>
2023-12-28 19:32:50 +0100

    python data structures - roman to integers

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import roman
-def roman_to_int(roman_string):
-    if type(roman_string) != str or type(roman_string) == None:
-        return 0
-    else:
-        return (roman.fromRoman(roman_string))
+if __name__ == "__main__":
+    import roman
+    def roman_to_int(roman_string):
+        if type(roman_string) != str or type(roman_string) == None:
+            return 0
+        else:
+            return (roman.fromRoman(roman_string))
