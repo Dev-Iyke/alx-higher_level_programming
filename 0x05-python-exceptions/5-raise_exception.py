c07b376e684b00fa38e8d51215f9558ff900df51
Dev-Iyke <iyke.thompson01@gmail.com>
2024-01-03 13:03:29 +0100

    python exceptions all other tasks

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
 def raise_exception():
+    a = 2
+    b = 'three'
+    return a + b
+    
--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
+    try:
+        a = 2
+        b = 3
+        a + b
+        return (r)
+    except NameError:
+        print(message)
