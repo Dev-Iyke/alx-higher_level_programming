3ccb252e9160205687cb1638cc00d4359573b132
Dev-Iyke <iyke.thompson01@gmail.com>
2024-01-04 21:55:19 +0100

    python Classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Here I am adding a private variable
+
+""" Here is the Square """
+
+
+class Square:
+    """ Initializing the class """
+    def __init__(self, size):
+        """ creating a private attribute """
+        self.__size = size
