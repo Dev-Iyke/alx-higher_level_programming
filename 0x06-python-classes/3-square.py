9156196a13a93108fb2179f45846b714ac6c070b
Dev-Iyke <iyke.thompson01@gmail.com>
2024-01-04 22:37:05 +0100

    python Classes

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# Here I am adding a private variable
+
+""" Here is the Square """
+
+
+class Square:
+    """ Initializing the class """
+    def __init__(self, size=0):
+        """ creating a private attribute """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        
+    def area(self):
+        return (self.__size * self.__size)
