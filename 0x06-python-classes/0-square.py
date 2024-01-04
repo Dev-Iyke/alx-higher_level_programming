8ece0530fa26cdf2e21eebc41f7c09390927e790
Dev-Iyke <iyke.thompson01@gmail.com>
2024-01-04 19:21:24 +0100

    python Classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+# I am creating an empty square class
+
+""" Defining the square """
+
+
 class Square:
+    """ Initializing the class """
     def __init__(self):
+        """ the class will do nothing """
         pass
